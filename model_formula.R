bffa0bf5f6ec5be745d7ebfb938f3570fb8c9a46
marinayxh <54040070+marinayxh@users.noreply.github.com>
2020-10-03 21:23:00 -0500

    Add files via upload

---
+++ model_formula.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+
+
+avg_duration_per_SNF_CT <- read_csv("D:/fall2020/case study/case study 1/avg_duration_per_SNF_CT.csv")
+
+
+calc_CCs <- function (pat_num, CT1_prop, CT2_prop, PID, df = avg_duration_per_SNF_CT){
+  if (pat_num < 0){
+    return("Negative value is not acceptable. Please enter a valid number.")
+  } 
+  if ((CT1_prop*0.01 > 1|CT1_prop*0.01 < 0)|(CT2_prop*0.01 > 1|CT2_prop*0.01 < 0)){
+    return("Proportion ranges from (0,100). Please enter a valid number.")
+  }
+  
+  
+  avg_CT1_time = as.numeric(avg_duration_per_SNF_CT[avg_duration_per_SNF_CT$PID == PID & avg_duration_per_SNF_CT$ContractType == "CT1", "average_time_spent"])
+  avg_CT2_time = as.numeric(avg_duration_per_SNF_CT[avg_duration_per_SNF_CT$PID == PID & avg_duration_per_SNF_CT$ContractType == "CT2", "average_time_spent"])
+  
+
+  CCs_num = (((pat_num*(CT1_prop*0.01)*avg_CT1_time)/60)/52)/40 + (((pat_num*(CT2_prop*0.01)*avg_CT2_time)/60)/52)/40
+ 
+  return(ceiling(CCs_num))
+    
+}
+
